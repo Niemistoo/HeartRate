d5b5be7ba22d67c5cb9bd354bbe8a148c2b0d2ce
Niemistoo <jappe.nie@gmail.com>
2025-01-08 17:30:05 +0200

    Simple heart rate calculator

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+Mobile and hybrid technologies Task 1 - Heart Rate Calculator
+*/
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculate = () => {
+    const numericAge = parseInt(age);
+
+    if (isNaN(numericAge)) {
+      setResult('Invalid Input');
+      return;
+    }
+
+    const lower = (220 - numericAge) * 0.65;
+    const upper = (220 - numericAge) * 0.85;
+    setResult(`${lower.toFixed(0)} - ${upper.toFixed(0)}`);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+        style={styles.field}
+        placeholder="Enter your age"
+        onChangeText={e => setAge(e)}
+        keyboardType='numeric'
+      />
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{result}</Text>
+      <Button title='Calculate' onPress={calculate}></Button>
     </View>
   );
 }
@@ -13,8 +40,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 50,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
+  field: {
+    margin: 50,
+    fontSize: 40,
+    marginBottom: 10,
+
+  }
 });
